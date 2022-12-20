2ed07191dd91b81bc5bfc46314b0ccff2bfae0ea
hikari@sunlightfuture.co.uk <lhkkennedy@gmail.com>
2022-12-20 19:49:32 +0000

    inital

---
+++ App/static/script.js
@@ -0,0 +1,26 @@
+var audio = {
+    init: function() {
+    var $that = this;
+        $(function() {
+            $that.components.media();
+        })
+    },
+    components: {
+        media: function(target) {
+            var media = $('audio.fc-media', (target !== undefined) ? target : 'body');
+            if (media.length) {
+                media.mediaelementplayer({
+                    audioHeight: 40,
+                    features : ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
+                    alwaysShowControls: true,
+                    timeAndDurationSeparator: '<spand></span>',
+                    iPadUseNativeControls: true,
+                    IphoneUseNativeControls: true,
+                    AndroidUseNativeControls: true
+                });
+            }
+        }
+    }
+}
+
+audio.init();
